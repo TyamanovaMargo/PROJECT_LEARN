a3406f9184c98efc68a8ba97afb44fd0a79cf68d
Margo Tyamanova <margaretzav@mail.com>
2022-11-01 18:38:13 +0200

    First commit

--- main.py
+++ main.py
@@ -8,13 +8,28 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-
+    def rate_lectorer(self, lectorer, course, grade):
+        if isinstance(lectorer, Lecturer) and course in lectorer.courses_attached :
+            if course in lectorer.grades:
+                lectorer.grades[course] += [grade]
+            else:
+                lectorer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor): #эксперты, проверяющие домашние задания
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -25,17 +40,38 @@ class Mentor:
         else:
             return 'Ошибка'
 
+    def __str__(self,name,surname):
+        
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+# students
+best_student = Student('Ivanovich', 'Ivan', 'm')
 best_student.courses_in_progress += ['Python']
+best_student_girl = Student('Markova', 'Lidiya', 'f')
+best_student_girl.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# lectorers
+best_lectorer = Lecturer('Some', 'Buddy')
+best_lectorer.courses_attached += ['Python']
+best_lectorer_lead = Lecturer('Burkin', 'Gleb')
+best_lectorer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+#reviers
+best_reviewer = Reviewer('Sushkina', 'Nika')
+best_reviewer.courses_attached +=['Python']
+best_reviewer_teamlead = Reviewer('Korkina', 'Lida')
+best_reviewer.courses_attached +=['Python']
 
-print(best_student.grades)
+# method
+best_reviewer.rate_hw(best_student, 'Python', 10)
+best_reviewer.rate_hw(best_student, 'Python', 10)
+best_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lectorer(best_lectorer,'Python',15)
+best_student.rate_lectorer(best_lectorer,'Python',5)
+
+print(best_lectorer.grades)
+
+
+# print(student1.courses_in_progress)
 
 
